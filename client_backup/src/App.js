70b3da16dba89c1b805863409e452e223085e746
Turutupa <adc.batros@gmail.com>
2020-02-12 18:41:14 +0100

    big update

---
+++ client_backup/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the <code>weekly padel</code> app.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>My token = {window.token}</p>
+        <p>Is this working?</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- client_dev/src/components/UserInformation.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class UserInformation extends Component {
-  render() {
-    return <div>User Information!</div>;
-  }
-}
